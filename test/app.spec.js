ec8947a7b6772c9630203b29e9b5c66e73f0a370
Cody Gillette <cgillette1312@gmail.com>
2019-05-02 12:50:28 -0700

    updated

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,10 +1,10 @@
 'use strict';
-
+/*global request*/
 const app = require('../src/app');
 
 describe('App', () => {
   it('GET / responds with 200 containitng "Hello, world!" ', () => {
-    return supertest(app)
+    return request(app)
       .get('/')
       .expect(200, 'Hello, world!');
   });
--- test/setup.js
+++ test/setup.js
@@ -3,4 +3,4 @@ const { expect } = require('chai');
 const supertest = require('supertest');
 
 global.expect = expect;
-global.supertest = supertest;
+global.request = supertest;
